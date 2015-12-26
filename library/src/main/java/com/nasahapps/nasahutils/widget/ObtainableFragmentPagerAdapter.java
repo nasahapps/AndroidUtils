199ddd4a90ec0a86d1975655504da87c27211fba
Hakeem <nasahapps@gmail.com>
2015-12-26 10:52:41 -0800

    Added a FragmentPagerAdapter where you can obtain the fragment within it.

---
+++ library/src/main/java/com/nasahapps/nasahutils/widget/ObtainableFragmentPagerAdapter.java
@@ -0,0 +1,33 @@
+package com.nasahapps.nasahutils.widget;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.util.SparseArray;
+import android.view.ViewGroup;
+
+public abstract class ObtainableFragmentPagerAdapter extends FragmentStatePagerAdapter {
+
+    SparseArray<Fragment> registeredFragments = new SparseArray<>();
+
+    public ObtainableFragmentPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        Fragment fragment = (Fragment) super.instantiateItem(container, position);
+        registeredFragments.put(position, fragment);
+        return fragment;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        registeredFragments.remove(position);
+        super.destroyItem(container, position, object);
+    }
+
+    public Fragment getRegisteredFragment(int position) {
+        return registeredFragments.get(position);
+    }
+}
